4f80edcf2a2d9d340011bbbad1479b603badc811
Panos Kumasaka <35408966+KumasakaPanos@users.noreply.github.com>
2021-03-01 11:31:24 -0800

    Added the actual code
    Added the .py file that actually does the work.

---
+++ code.py
@@ -0,0 +1,20 @@
+import sys;
+
+def gIn():
+	try:
+		return(int(input()))
+	except ValueError:
+		print("Whole numbers only, please. \n");
+
+def oCalc(x):
+	cm = .9;
+	cTotal = 1;
+	
+
+	for j in range(x-1):
+		cTotal *= cm;
+		cm = cm - .1;
+
+	return(round((1-cTotal)*100));
+	
+print(oCalc(gIn()));
